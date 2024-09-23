9cd27bb36fe723d6fc7611f65fa49ded09df3f96
graceko <gracekodes@gmail.com>
2024-09-22 23:47:51 -0700

    Create realistic appearances of 3D object using materials

--- 11-materials/src/script.js
+++ 11-materials/src/script.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 
 /**
  * Base
@@ -10,15 +12,24 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
-//textures
+/**
+ * textures
+ */
 const textureLoader = new THREE.TextureLoader();
 const doorColorTexture = textureLoader.load('./textures/door/color.jpg');
 const matcapTexture = textureLoader.load('./textures/matcaps/2.png');
+const gradientTexture = textureLoader.load('./textures/gradients/5.jpg');
+const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg');
+const doorHeightTexture = textureLoader.load('./textures/door/height.jpg');
+const doorMetalnessTexture = textureLoader.load('./textures/door/metalness.jpg');
+const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg');
+const doorNormalTexture = textureLoader.load('textures/door/normal.jpg');
+const doorAlphaTexture = textureLoader.load('textures/door/alpha.jpg');
 doorColorTexture.colorSpace = THREE.SRGBColorSpace;
 
-/*
-Objects
-*/
+/**
+ * Objects
+ */
 // const material = new THREE.MeshBasicMaterial({
     //map: doorColorTexture, 
     //color: 'red',
@@ -36,17 +47,102 @@ Objects
 //     specular: new THREE.Color(0x1188ff)
 // });
 
+//Material Cartoon Effect
+// const material = new THREE.MeshToonMaterial({ 
+//     gradientMap: gradientTexture,
+// });
+// gradientTexture.minFilter = THREE.NearestFilter;
+// gradientTexture.magFilter = THREE.NearestFilter;
+// gradientTexture.generateMipmaps = false; 
 
+//MeshStandardMaterial
+// const material = new THREE.MeshStandardMaterial();
+// material.metalness = 1;
+// material.roughness = 1;
+// material.map = doorColorTexture;
+// material.aoMap = doorAmbientOcclusionTexture;
+// material.aoMapIntensity = 1;
+// material.displacementMap = doorHeightTexture;
+// material.displacementScale = 0.1; 
+// material.metalnessMap = doorMetalnessTexture;
+// material.roughnessMap = doorRoughnessTexture;
+// material.normalMap = doorNormalTexture;
+// material.normalScale.set(0.5, 0.5);
+// material.transparent = true;
+// material.alphaMap = doorAlphaTexture;
+
+//MeshPhysicalMaterial
+const material = new THREE.MeshPhysicalMaterial();
+material.metalness = 0;
+material.roughness = 0;
+// material.map = doorColorTexture;
+// material.aoMap = doorAmbientOcclusionTexture;
+// material.aoMapIntensity = 1;
+// material.displacementMap = doorHeightTexture;
+// material.displacementScale = 0.1; 
+// material.metalnessMap = doorMetalnessTexture;
+// material.roughnessMap = doorRoughnessTexture;
+// material.normalMap = doorNormalTexture;
+// material.normalScale.set(0.5, 0.5);
+// material.transparent = true;
+// material.alphaMap = doorAlphaTexture;
+
+//clearcoat
+//material.clearcoat = 1;
+//material.clearcoatRoughness = 0;
+
+//sheen (fluffy materials like fabric)
+// material.sheen = 1;
+// material.sheenRoughness = 0.25;
+// material.sheenColor.set(1, 1, 1);
+
+//iridescence (for textures like disk)
+// material.iridescence = 1;
+// material.iridescenceIOR = 1;
+// material.iridescenceThicknessRange = [100, 800];
+
+//transmission - see what's behind the object
+material.transmission = 1;
+material.ior = 1.5;
+material.thickness = 0.5;
+
+
+
+/**
+ * Debug
+ */
+const gui = new GUI();
+gui.add(material, 'metalness').min(0).max(1).step(0.0001);
+gui.add(material, 'roughness').min(0).max(1).step(0.0001);
+// gui.add(material, 'clearcoat').min(0).max(1).step(0.0001);
+// gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001);
+// gui.add(material, 'sheen').min(0).max(1).step(0.0001);
+// gui.addColor(material, 'sheenColor');
+// gui.add(material, 'iridescence').min(0).max(1).step(0.0001);
+// gui.add(material, 'iridescenceIOR').min(1).max(2.33).step(0.0001);
+// gui.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1);
+// gui.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1);
+
+gui.add(material, 'transmission').min(0).max(10).step(0.0001);
+
+// ior - index of refraction, feel translucent (e.g. diamond: 2.417)
+gui.add(material, 'ior').min(0).max(10).step(0.0001);
+gui.add(material, 'thickness').min(0).max(1).step(0.0001);
+
+
+/**
+ * Mesh Gemoetries 
+ */
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16), 
+    new THREE.SphereGeometry(0.5, 64, 64), 
     material 
 );
 const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(1, 1), 
+    new THREE.PlaneGeometry(1, 1, 100, 100), 
     material
 );
 const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 16, 32), 
+    new THREE.TorusGeometry(0.3, 0.2, 64, 128), 
     material
 );
 scene.add(sphere, plane, torus);
@@ -54,7 +150,9 @@ scene.add(sphere, plane, torus);
 sphere.position.x = -1.5;
 torus.position.x = 1.5;
 
-//Lights
+/**
+ * Lights
+ */
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
 scene.add(ambientLight);
 const pointLight = new THREE.PointLight(0xffffff, 30);
@@ -62,7 +160,15 @@ pointLight.position.x = 2;
 pointLight.position.y = 3;
 pointLight.position.z = 4;
 scene.add(pointLight);
-
+/**
+ * Environment map
+ */
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load('./textures/environmentMap/2k.hdr', (environmentMap) => {
+    environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = environmentMap;
+    scene.environment = environmentMap;
+});
 /**
  * Sizes
  */
@@ -71,6 +177,9 @@ const sizes = {
     height: window.innerHeight
 };
 
+/**
+ * Resizing
+ */
 window.addEventListener('resize', () =>
 {
     // Update sizes
@@ -85,7 +194,6 @@ window.addEventListener('resize', () =>
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
-
 
 /**
  * Camera
